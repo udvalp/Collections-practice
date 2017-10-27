88bc09538d8ddf1f1b5975541be920851382671a
Udval Panchlothia <udval.ce@gmail.com>
2017-10-27 09:31:17 -0700

    Arraylist

--- src/arrayTest.java
+++ src/arrayTest.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class arrayTest {
 
@@ -8,11 +10,12 @@ public class arrayTest {
 		int[] test1 = {3,4,5};
 		int abc = test1.hashCode();
 		String new1 = test1.toString();
+		ArrayList<int[]> AL = (ArrayList<int[]>) Arrays.asList(test1);
 		for(int test2: test1) {
 			
 			System.out.println(test2);
 			System.out.println(abc);
-			System.out.println(Arrays.toString(test1));
+			System.out.println("nuetral");
 		}
 	}
 
